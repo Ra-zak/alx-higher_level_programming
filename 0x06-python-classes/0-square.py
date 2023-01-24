75b27276b60a903259d36c84c5e19136eced7a01
Ra-zak <113684251+Ra-zak@users.noreply.github.com>
2023-01-24 12:29:32 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Square Class
+"""Empty class Square that defines a square"""
 
-This class don't do anything for the moment
-
-"""
 
 class Square:
     """Empty class Square that defines a square"""
