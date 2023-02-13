d5b0e2c92e707b039557910390f1d1381301d5fe
Ra-zak <113684251+Ra-zak@users.noreply.github.com>
2023-02-13 23:12:12 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+module
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """
+    class
+    """
     def __init__(self, width, height):
+        """
+        functio def
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
