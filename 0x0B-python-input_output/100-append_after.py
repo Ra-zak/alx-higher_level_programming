cfb16fe8c81248d7326ff8b4754b7c795b3bf279
Ra-zak <113684251+Ra-zak@users.noreply.github.com>
2023-02-13 22:39:22 +0100

    Add files via upload

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+function definition
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    func definitiong and calib
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
+        text = f.readlines()
+        new_text = []
+
+        for line in text:
+            new_text.append(line)
+
+            if search_string in line:
+                new_text.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as nf:
+        for line in new_text:
+            nf.write(line)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a module that returns the dict
+"""
+
+
+def class_to_json(obj):
+    """
+    function that returns dict
+    """
+    return obj.__dict__
